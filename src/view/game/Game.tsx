272e3193247fa1db7dcf307df0f46e4fc0ad6ff8
Joe Grundman <jonathangrundman@hotmail.com>
2018-08-19 20:08:21 +0100

    handle events on switching theaters

--- src/view/game/Game.tsx
+++ src/view/game/Game.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import MainMap, { IMapOpts } from '../../components/mainMap/MainMap'
 import { urlMapEur, urlMapPac } from '../../constants/ui-constants'
-import { handleKeyboardEvent, handleKeyupEvent }from '../../wp/Eventing/eventing-keyboard'
+import { handleKeyboardEvent, handleKeyupEvent } from '../../wp/Eventing/eventing-keyboard'
 import { getGame } from '../../wp/Game'
 import Game from '../../wp/Game/game'
 import { initialize } from '../../wp/init/init'
@@ -9,24 +9,37 @@ import locals from './Game.css'
 
 interface IGameState {
   theater: string
-  mapCanvas: HTMLCanvasElement | null
-  mapCtx: CanvasRenderingContext2D | null
   initialized: boolean
 }
 
 class GameBoard extends React.Component<{}, IGameState> {
   public mainMapContainer: React.RefObject<HTMLDivElement>
+  public game: Game | null
+  public mapCanvas: HTMLCanvasElement | null
+  public mapCtx: CanvasRenderingContext2D | null
+  public doubleClickListener: EventListenerOrEventListenerObject | null
+  public mouseDownListener: EventListenerOrEventListenerObject | null
+  public mouseMoveListener: EventListenerOrEventListenerObject | null
+  public mouseUpListener: EventListenerOrEventListenerObject | null
+  public scrollListener: EventListenerOrEventListenerObject | null
 
-   constructor(props: {}) {
+  constructor(props: {}) {
     super(props)
+
     this.state = {
       initialized: false,
-      mapCanvas: null,
-      mapCtx: null,
       theater: urlMapEur,
     }
 
+    this.game = null
+    this.mapCanvas = null
+    this.mapCtx = null
     this.mainMapContainer = React.createRef()
+    this.doubleClickListener = null
+    this.mouseDownListener = null
+    this.mouseMoveListener = null
+    this.mouseUpListener = null
+    this.scrollListener = null
   }
 
   public attachKeyboardEvents() {
@@ -35,48 +48,68 @@ class GameBoard extends React.Component<{}, IGameState> {
   }
 
   public attachMapEvents() {
-    const game: Game = getGame()
 
-		if (this.state.mapCanvas && this.state.mapCanvas.addEventListener) {
-			this.state.mapCanvas.addEventListener('mousemove', game.currentMap.onMouseMove.bind(game.currentMap), false)
-      this.state.mapCanvas.addEventListener('mousedown', game.currentMap.onMouseDown.bind(game.currentMap), false)
-      this.state.mapCanvas.addEventListener('mouseup', game.currentMap.onMouseUp.bind(game.currentMap), false)
-      this.state.mapCanvas.addEventListener('dblclick', game.currentMap.onDoubleClick.bind(game.currentMap), false)
-      if(this.mainMapContainer.current) {
-        this.mainMapContainer.current.addEventListener('scroll', game.currentMap.onScroll.bind(game.currentMap), true)
+    if (this.game && this.mapCanvas && this.mapCanvas.addEventListener) {
+      this.mouseDownListener = this.game.currentMap.onMouseDown.bind(this.game.currentMap) as EventListenerOrEventListenerObject
+      this.mouseMoveListener = this.game.currentMap.onMouseMove.bind(this.game.currentMap) as EventListenerOrEventListenerObject
+      this.mouseUpListener = this.game.currentMap.onMouseUp.bind(this.game.currentMap) as EventListenerOrEventListenerObject
+      this.scrollListener = this.game.currentMap.onScroll.bind(this.game.currentMap) as EventListenerOrEventListenerObject
+      this.doubleClickListener = this.game.currentMap.onDoubleClick.bind(this.game.currentMap) as EventListenerOrEventListenerObject
+
+      this.mapCanvas.addEventListener('mousemove', this.mouseMoveListener, false)
+      this.mapCanvas.addEventListener('mousedown', this.mouseDownListener, false)
+      this.mapCanvas.addEventListener('mouseup', this.mouseUpListener, false)
+      this.mapCanvas.addEventListener('dblclick', this.doubleClickListener, false)
+      if (this.mainMapContainer.current) {
+        this.mainMapContainer.current.addEventListener('scroll', this.scrollListener, true)
       }
-		}
-	}
+
+    }
+  }
+
+  public removeMapEvents(): void {
+
+    if (this.game && this.mapCanvas && this.mapCanvas.removeEventListener) {
+      this.mapCanvas.removeEventListener('mousemove', this.mouseMoveListener as EventListenerOrEventListenerObject, false)
+      this.mapCanvas.removeEventListener('mousedown', this.mouseDownListener as EventListenerOrEventListenerObject, false)
+      this.mapCanvas.removeEventListener('mouseup', this.mouseUpListener as EventListenerOrEventListenerObject, false)
+      this.mapCanvas.removeEventListener('dblclick', this.doubleClickListener as EventListenerOrEventListenerObject, false)
+      if (this.mainMapContainer.current) {
+        this.mainMapContainer.current.removeEventListener('scroll', this.scrollListener as EventListenerOrEventListenerObject, true)
+      }
+      this.game.currentMap.mapCanvas = this.mapCanvas
+    }
+
+  }
 
   public getMapContext = (mapCanvas: HTMLCanvasElement, mapCtx: CanvasRenderingContext2D, opts: IMapOpts) => {
-    this.setState({
-      mapCanvas, mapCtx
-    },
-     () => {
+    this.mapCanvas = mapCanvas
+    this.mapCtx = mapCtx
 
-      if(this.state.mapCtx && this.state.mapCanvas) {
-
-        /**
-         * Initialize the wp game instance and start the scripts
-         */
-        if (!this.state.initialized) {
-          initialize(this.state.mapCtx, this.state.mapCanvas, opts)
-          this.attachMapEvents()
-          this.attachKeyboardEvents()
-          this.setState({
-            initialized: true
-          })}
-        else {
-          getGame().switchTheaters()
-        }  
+    /**
+     * Initialize the wp this.state.game instance and start the scripts
+     */
+    if (!this.state.initialized) {
+      initialize(this.mapCtx, this.mapCanvas, opts)
+      this.game = getGame()
+      this.attachMapEvents()
+      this.attachKeyboardEvents()
+      this.setState({
+        initialized: true
+      })
+    }
+    else {
+      if (this.game) {
+        this.game.switchTheaters()
+        this.attachMapEvents()
       }
-    })
+    }
   }
 
   public toggleMap = () => {
     const isEur = this.state.theater === urlMapEur
     const theater = isEur ? urlMapPac : urlMapEur
-    
+    this.removeMapEvents()
     this.setState({
       theater
     })
@@ -89,12 +122,12 @@ class GameBoard extends React.Component<{}, IGameState> {
     return (
       <div className={locals.main}>
         <button onClick={this.toggleMap}>toggle map</button>
-          <div ref={this.mainMapContainer} className={locals.mapContainer}>
-            <MainMap getMapContext={this.getMapContext} url={map}/>
-          </div>
+        <div ref={this.mainMapContainer} className={locals.mapContainer}>
+          <MainMap getMapContext={this.getMapContext} url={map} />
+        </div>
       </div>
     )
   }
-};
+}
 
 export default GameBoard
