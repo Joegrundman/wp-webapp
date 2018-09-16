44c15e053675a48618cb4c155062b418ae525d93
Joe Grundman <jonathangrundman@hotmail.com>
2018-09-16 18:43:04 +0100

    add canvas and background for taskforce dialog display

--- src/components/taskforce/Taskforce.css.d.ts
+++ src/components/taskforce/Taskforce.css.d.ts
@@ -2,6 +2,8 @@ export interface ITaskforceCss {
   'header': string
   'subheaderWithArrows': string
   'subheaderCenterText': string
+  'canvasContainer': string
+  'taskforceCanvas': string
 }
 
 export const locals: ITaskforceCss;
--- src/components/taskforce/Taskforce.tsx
+++ src/components/taskforce/Taskforce.tsx
@@ -17,12 +17,54 @@ const countries: string[]= [
 ]
 
 class Taskforce extends React.Component<{}, ITaskforceState> {
+  public tfBgContext: CanvasRenderingContext2D
+  public tfContext: CanvasRenderingContext2D
 
   constructor(props: any) {
     super(props)
     this.state = {
       currentCountry: 0
     }
+  }
+
+  public componentDidMount (): void {
+    const tfBgCanvas: HTMLCanvasElement = document.getElementById('taskforceCanvasBackground') as HTMLCanvasElement
+    this.tfBgContext = tfBgCanvas.getContext('2d') as CanvasRenderingContext2D
+    const tfCanvas: HTMLCanvasElement = document.getElementById('taskforceCanvas') as HTMLCanvasElement
+    this.tfContext = tfCanvas.getContext('2d') as CanvasRenderingContext2D
+    this.drawTaskforceBackground();
+    // getGame().setSelectedShipyard(shipyards[this.state.currentShipyard], this.syContext)
+  }
+
+  public drawTaskforceBackground (): void {
+    const tfLineHeight = 62.5
+    const tfNumberCol = 32.5
+    const numRows = 12
+
+    this.tfBgContext.strokeStyle= '#cdcdcd'
+    this.tfBgContext.lineWidth = 1
+
+    this.tfBgContext.beginPath()
+    this.tfBgContext.moveTo(tfNumberCol, 0)
+    this.tfBgContext.lineTo(tfNumberCol, numRows * tfLineHeight)
+    this.tfBgContext.closePath()
+    this.tfBgContext.stroke()
+
+    for (let i: number = 1; i < numRows; i++) {
+      
+      this.tfBgContext.beginPath()
+      this.tfBgContext.moveTo(0, i * tfLineHeight)
+      this.tfBgContext.lineTo(500, i * tfLineHeight)
+      this.tfBgContext.closePath()
+      this.tfBgContext.stroke()
+    }
+
+    const taskforces: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+    this.tfBgContext.font = "18px sans-serif"
+    this.tfBgContext.fillStyle = '#cdcdcd'
+    taskforces.forEach((tf: string, i: number): void => {
+      this.tfBgContext.fillText(tf, i < 9 ? 10 : 5, 40.5 + (tfLineHeight * i))
+    })
   }
 
   public changeCountryBack = () => {
@@ -45,6 +87,16 @@ class Taskforce extends React.Component<{}, ITaskforceState> {
   }
 
   public render () {
+
+    const tfContainerSize = {
+      height: 470,
+      width: 300
+    }
+
+    const tfCanvasSize = {
+      height: 12 * 62.5,
+      width: 500
+    }
     return (
       <div>
         <h1 className={locals.header} >Taskforces</h1>
@@ -52,6 +104,18 @@ class Taskforce extends React.Component<{}, ITaskforceState> {
           <ArrowButton direction="left" action={this.changeCountryBack}/>
           <h2 className={locals.subheaderCenterText}>{countries[this.state.currentCountry]}</h2>
           <ArrowButton direction="right" action={this.changeCountryForward}/>
+        </div>
+        <div className={locals.canvasContainer} style={{height: tfContainerSize.height, width: tfContainerSize.width}}>
+          <canvas
+            className={locals.taskforceCanvas}
+            id="taskforceCanvasBackground"
+            height={tfCanvasSize.height}
+            width={tfCanvasSize.width}/>
+          <canvas
+            className={locals.taskforceCanvas}
+            id="taskforceCanvas"
+            height={tfCanvasSize.height}
+            width={tfCanvasSize.width}/>
         </div>
       </div>
     )
